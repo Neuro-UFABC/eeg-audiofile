302f02d8735de480ba7db30a690a805474fe5862
Bóris Marin <borismarin@gmail.com>
2022-06-24 18:37:32 -0300

    arrumando scripts

---
+++ psych/audiofile.m
@@ -0,0 +1,41 @@
+1;
+
+function [wptr, wrect] = inicializa_display()
+    showStatusScreen = true;
+
+    whichScreen=max(Screen('Screens'));
+    PsychImaging('PrepareConfiguration');
+    PsychImaging('AddTask', 'FinalFormatting', 'DisplayColorCorrection', 'ClampOnly');
+    PsychImaging('AddTask', 'General', 'EnableBits++Bits++Output');
+    [wptr,wrect] = PsychImaging('OpenWindow', whichScreen);
+
+    if showStatusScreen
+        BitsPlusPlus('SwitchToStatusScreen');
+        % Wait for the switch to complete
+        pause(1);
+    endif
+endfunction
+
+
+
+function toca_faixa(wptr, faixa)
+   debug = false;
+  
+   preSelectTime = 10; % unidade: 100uS (10 = 1 ms)
+   triggerDuration = 10; % unidade 100uS (10 = 1 ms).
+
+   BitsPlusPlus('DIOCommand', wptr, 1, 511, [ones(1,preSelectTime)*faixa ones(1,triggerDuration)*(256+faixa) zeros(1,248-(triggerDuration+preSelectTime))], 0, 0, 2);
+   disp(['Tocando faixa: ',num2str(faixa)] )
+   Screen('Flip', wptr);
+
+   if debug
+      DrawFormattedText(wptr,['Foi enviado trigger ',num2str(faixa)],'center','center');
+      Screen('Flip', wptr);
+   endif
+
+endfunction
+
+
+toca_faixa(inicializa_display(), 1);
+
+sca;
--- psych/ca.m
+++
@@ -1,5 +0,0 @@
-whichScreen=max(Screen('Screens'));
-PsychImaging('PrepareConfiguration');
-PsychImaging('AddTask', 'FinalFormatting', 'DisplayColorCorrection', 'ClampOnly');
-PsychImaging('AddTask', 'General', 'EnableBits++Bits++Output');
-[wptr,wrect] = PsychImaging('OpenWindow', whichScreen, [128 128 128], [0 0 1000 1000]);
