8547b28c228ffe73e0f127b63ba321a7a580ad32
Bóris Marin <borismarin@gmail.com>
2022-06-27 15:26:43 -0300

    organizando experimento raul

--- psychopy/teste.py
+++ psychopy/teste.py
@@ -3,3 +3,5 @@ from psychopy.hardware import crs
 # we need to be rendering to framebuffer
 win = visual.Window([1024,768], useFBO=True)
 bits = crs.BitsSharp(win, mode = 'mono++', portName='/dev/ttyACM1')
+bits.mode = 'color++'
+
